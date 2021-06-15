82e4f3906b53abd9f676654b8b520d760c900740
Jantsen Tanner <jantsentanner@gmail.com>
2021-06-15 15:28:18 -0600

    added the User Model

--- JamSession/JamSession/Models/User.swift
+++ JamSession/JamSession/Models/User.swift
@@ -5,4 +5,26 @@
 //  Created by Jantsen Tanner on 6/15/21.
 //
 
-import Foundation
+import UIKit
+
+class User {
+    
+    let username: String
+    let profilePic: UIImage
+    let location: String
+    let bio: String?
+    let instrument: String
+    let experienceLevel: String
+    
+    init(username: String, profilePic: UIImage, location: String, bio: String, instrument: String, experienceLevel: String){
+        self.username = username
+        self.profilePic = profilePic
+        self.location = location
+        self.bio = bio
+        self.instrument = instrument
+        self.experienceLevel = experienceLevel
+    }
+}// End of class
+
+
+
