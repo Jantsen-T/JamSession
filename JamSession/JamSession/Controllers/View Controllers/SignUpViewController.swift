95110e1d94f08fe328d89000cd82434b5ca2f18f
Jantsen Tanner <jantsentanner@gmail.com>
2021-06-16 15:51:39 -0600

    adjusted the signup button tapped

--- JamSession/JamSession/Controllers/View Controllers/SignUpViewController.swift
+++ JamSession/JamSession/Controllers/View Controllers/SignUpViewController.swift
@@ -10,23 +10,20 @@ import FirebaseAuth
 import Firebase
 
 class SignUpViewController: UIViewController {
-
-    @IBOutlet weak var firstNameTextField: UITextField!
     
+    @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
+
     func validateFields() -> String? {
         //Check that all fields are filled in
         if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
             return "Please fill in all fields"
-            
         }
         //check if password is secure
         let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -34,15 +31,12 @@ class SignUpViewController: UIViewController {
             // Password isnt secure enough
             return " Please make sure password has at least 8 characters, contains a special character and a number."
         }
-        
         return nil
     }
-    
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
         
         let error = validateFields()
-        
         if let error = error {
             // there is something wrong with the fields, show error message
             showError(error)
@@ -53,51 +47,41 @@ class SignUpViewController: UIViewController {
             let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
             let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
             
-            
-            
             Auth.auth().createUser(withEmail: email, password: password) { result, error in
-                if error != nil {
+                if let error = error {
+                    print ("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
                     self.showError("Error creating user")
                 }
                 else {
                     //User created successfully
-                    
                     let db = Firestore.firestore()
-                    
-                    db.collection("Users").addDocument(data: ["Firstname" : firstName, "uid" : result!.user.uid]) { error in
+                    db.collection("Users").document(result!.user.uid).setData(["Firstname" : firstName, "uid" : result!.user.uid]) { error in
                         if error != nil {
                             self.showError("error saving user data")
                         }
                     }
-                    
-                    //transition to home screen
+                    //transition to home screen in the UI after the user was created succssfully
                     self.transitionToHome()
                 }
             }
         }
     }
     
-    
     func showError(_ message: String) {
-        
         errorLabel.text = message
         errorLabel.alpha = 1
-        
     }
     
     func transitionToHome() {
         
     }
-    
-    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
