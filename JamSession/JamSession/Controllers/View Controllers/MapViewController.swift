0699feeb6d2a7150323d99e070bc6fe9f4c9b166
tanmanroberts <tanmanroberts@gmail.com>
2021-06-21 10:38:14 -0600

    migrated over the working map function from the test app. Everything works and now just waiting to have the storyboard built out on the main file.

---
+++ JamSession/JamSession/Controllers/View Controllers/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  JamSession
+//
+//  Created by Tanner Roberts on 6/21/21.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
