3e34415440de4bff8b10495f0ff1aa87e09bde82
tanmanroberts <tanmanroberts@gmail.com>
2021-06-21 16:40:15 -0600

    added the event model and the corresponding event View C

---
+++ JamSession/JamSession/Models/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  JamSession
+//
+//  Created by Tanner Roberts on 6/21/21.
+//
+
+import Foundation
+
+struct Event {
+    
+    let eventName: String
+    let eventLocation: String
+    let eventTime: Data
+    let instruments: String
+    let eventDetails: String
+    
+}//End of struct
