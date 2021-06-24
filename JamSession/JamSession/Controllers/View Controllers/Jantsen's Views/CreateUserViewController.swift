5efaf6ae2dc47a2951be08ce1e4fc8323a2db05d
Jantsen Tanner <jantsentanner@gmail.com>
2021-06-24 12:05:12 -0600

    detled the login viewcontroller that I didnt need.

--- JamSession/JamSession/Controllers/View Controllers/Jantsen's Views/CreateUserViewController.swift
+++ JamSession/JamSession/Controllers/View Controllers/Jantsen's Views/CreateUserViewController.swift
@@ -24,36 +24,37 @@ class CreateUserViewController: UIViewController {
         pickerData = ["Beginner", "Intermediate", "advanced", "Expert"]
     }
     
+    var currentUser: User?
     
-//    @IBAction func createProfileButtonTapped(_ sender: Any) {
-//        guard let username = usernameTextField.text,
-//              let profilePic = profilePicImageView.image,
-//              let location = locationTextField.text,
-//              let experience = experienceLevelPicker.dataSource,
-//              let bio = bioTextView.text,
-//              let instrument = instrumentTextField.text else {return}
-//
-//        UserController.sharedInstance.makeUserInDB(username: username, uuid: , location: location, bio: bio, instrument: instrument, experience: experience as! String) { result in
-//            
-//        }
-//
-//
-//        //transitionToHome()
-//    }
-//
-//
-    func transitionToHome(){
-        //Call this function instead of a segue?
+    @IBAction func createProfileButtonTapped(_ sender: Any) {
+        guard let username = usernameTextField.text,
+              let profilePic = profilePicImageView.image,
+              let location = locationTextField.text,
+              let experience = experienceLevelPicker.dataSource,
+              let bio = bioTextView.text,
+              let instrument = instrumentTextField.text else {return}
+        
+        UserController.sharedInstance.makeUserInDB(username: username, uuid: , location: location, bio: bio, instrument: instrument, experience: experience) { result in
+            <#code#>
+        }
         
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    
+    //transitionToHome()
+}
+
+
+func transitionToHome(){
+    //Call this function instead of a segue?
     
 }
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
