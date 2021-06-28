542766182c9105f45907a5676930724b28335600
Jantsen Tanner <jantsentanner@gmail.com>
2021-06-28 14:56:26 -0600

    created a friend detail wire fram and its view controller file

---
+++ JamSession/JamSession/Controllers/View Controllers/jants/FriendDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FriendDetailViewController.swift
+//  JamSession
+//
+//  Created by Jantsen Tanner on 6/28/21.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+    @IBOutlet weak var friendUsername: UILabel!
+    @IBOutlet weak var friendPic: UIImageView!
+    @IBOutlet weak var friendLocation: UILabel!
+    @IBOutlet weak var friendInstrument: UILabel!
+    @IBOutlet weak var friendExperience: UILabel!
+    @IBOutlet weak var friendBio: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
